fd8639a5e3d2c8baef0c691dba65fc3777858c59
Alban <kingenalban@gmail.com>
2019-04-29 21:27:34 +0200

    usps and dogs post type

---
+++ global-templates/dogs.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Portfolio Items setup.
+ *
+ * @package understrap
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+// Get the three latest Portfolio Items
+$dogs = new WP_Query([
+	'post_type' => 'animal_dogs',
+	'posts_per_page' => -1,
+]);
+
+// Did we get any Portfolio Items?
+if ($dogs->have_posts()) {
+	// GREAT SUCCESS!
+	?>
+		<div class="wrapper" id="wrapper-dogs">
+			<div class="container">
+
+				<h1 style="color:black;"><?php _e('Senaste Hundar', 'understrap'); ?></h1>
+
+				<div class="row">
+					<!-- Loop over the Portfolio Items -->
+					<?php
+						while ($dogs->have_posts()) {
+							$dogs->the_post();
+							?>
+								<!-- For each Portfolio Item, include a template part? -->
+								<?php get_template_part('loop-templates/content-dog'); ?>
+							<?php
+						}
+
+						// DON'T FORGET TO RESET POSTDATA!
+						wp_reset_postdata();
+					?>
+				</div><!-- /.row -->
+			</div><!-- /.container -->
+		</div><!-- /#wrapper-portfolio-items -->
+	<?php
+}
---
+++ loop-templates/content-dog.php
@@ -0,0 +1,46 @@
+<div class="wrapper-dog col-md-6 col-lg-4">
+    <article class="dog">
+        <header>
+            <?php if (has_post_thumbnail()) : ?>
+                <a href="<?php the_permalink(); ?>">
+                    <?php the_post_thumbnail('portfolio-thumbnail', ['class' => 'img-fluid']); ?>
+                </a>
+            <?php endif; ?>
+
+            <h1><?php the_title(); ?></h1>
+        </header>
+
+        <main>
+            <?php the_excerpt(); ?>
+        </main>
+        <div class="card-footer">
+            <small class="text-muted">
+                <?php
+                    printf(
+                        __('Post Created: %s by %s', 'understrap'),
+                        get_the_date(),
+                        get_the_author_posts_link(),
+                    );
+                ?>
+            </small>
+        </div><!-- /.card-footer -->
+        <footer>
+
+            <?php
+                if ($client = get_post_meta(get_the_ID(), 'client', true)) {
+                    printf(__('<div class="client">Client: %s</div>', 'understrap'), $client);
+                }
+            ?>
+
+            <div class="branch">
+                <?php
+                    the_terms(
+                        get_the_ID(),
+                        'us_portfolio_branch',
+                        __('Branch: ', 'understrap')
+                    );
+                ?>
+            </div><!-- /.branch -->
+        </footer>
+    </article>
+</div>
